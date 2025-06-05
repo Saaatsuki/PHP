9b4c54ed02636ca058cf2e86b2befeb3087fe9e3
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2025-06-05 21:53:37 +0900

    php 2-8

--- Progate/02/index.php
+++ Progate/02/index.php
@@ -2,19 +2,22 @@
 class Menu {
   public $name;
   
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  
   public function hello() {
-    // '私は○○です'とechoしてください
-    echo '私は'.$this -> name.'です';
-    
+    echo '私は'.$this->name.'です';
   }
 }
 
-$curry = new Menu();
-$pasta = new Menu();
-$curry -> name = 'CURRY';
-$pasta -> name = 'PASTA';
-$curry -> hello();
-echo '<br>';
-$pasta -> hello();
+$curry = new Menu('CURRY');
+$pasta = new Menu('PASTA');
 
 ?>
+
+<!-- <p>タグの中で、$curryのnameプロパティを表示してください -->
+<p><?php echo $curry -> name ?></p>
+
+<!-- <p>タグの中で、$pastaのnameプロパティを表示してください -->
+<p><?php echo $pasta -> name ?></p>
