eb1cf4fae6f5faa1fa5c2a64813c8bf566ca55c0
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2025-07-10 17:38:31 +0900

    250710

---
+++ 철문/250706/01/src/index04.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Form Example</title>
+</head>
+<body>
+  <form action="">
+    ID : <input type="text" name="id"><br>
+    PW : <input type="password" name="pw"><br>
+    <input type="submit" value="전송">
+  </form>
+</body>
+</html>
